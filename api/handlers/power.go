a75b11cb8cff13331f0f7244fdb79fed789261a1
Frédéric Thomas <frederic.thomas2@icloud.com>
2023-04-11 15:46:30 +0200

    feat(power): reset + soft

--- api/handlers/power.go
+++ api/handlers/power.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/Bancadati/ipmitool"
 	"github.com/gin-gonic/gin"
+	"github.com/squarefactory/ipmitool"
 )
 
 func PowerOn(c *gin.Context) {
@@ -85,4 +85,58 @@ func Cycle(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Compute node already powered off"})
 	}
 
+}
+
+func Soft(c *gin.Context) {
+	hostname := c.Param("host")
+	cl, err := ipmitool.NewClient(hostname, 0, "IPMIUSER", "Password")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	status, err := cl.Power.Status()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if status == ipmitool.PowerStateOn {
+		err := cl.Power.Soft()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Success"})
+
+	} else {
+		c.JSON(http.StatusOK, gin.H{"message": "Compute node already powered off"})
+	}
+}
+
+func Reset(c *gin.Context) {
+	hostname := c.Param("host")
+	cl, err := ipmitool.NewClient(hostname, 0, "IPMIUSER", "Password")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	status, err := cl.Power.Status()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if status == ipmitool.PowerStateOn {
+		err := cl.Power.Reset()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Success"})
+
+	} else {
+		c.JSON(http.StatusOK, gin.H{"message": "Compute node already powered off"})
+	}
 }
--- api/handlers/status.go
+++ api/handlers/status.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/Bancadati/ipmitool"
 	"github.com/gin-gonic/gin"
+	"github.com/squarefactory/ipmitool"
 )
 
 func Status(c *gin.Context) {
