dedcedc26aa0758d2c3cba794d050b4c72dfcc90
Frédéric Thomas <frederic.thomas2@icloud.com>
2023-04-17 21:38:52 +0200

    feat: health handler

--- main.go
+++ main.go
@@ -15,9 +15,10 @@ func main() {
 	r.POST("/host/:host/on", handlers.PowerOn)
 	r.POST("/host/:host/off", handlers.PowerOff)
 	r.POST("/host/:host/cycle", handlers.Cycle)
-	r.GET("/host/:host/status", handlers.Status)
+	r.POST("/host/:host/status", handlers.Status)
 	r.POST("/host/:host/soft", handlers.Soft)
 	r.POST("/host/:host/reset", handlers.Reset)
+	r.GET("/health", handlers.Health)
 
 	listenAddress := os.Getenv("LISTEN_ADDRESS")
 	if len(listenAddress) == 0 {
